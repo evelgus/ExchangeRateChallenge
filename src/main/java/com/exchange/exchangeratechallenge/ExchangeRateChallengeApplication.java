0a8d0ae916cb1c655d76b1c4ddc796d0477faea8
evelgus <velhus@ukr.net>
2025-03-03 15:48:52 +0200

    create project

---
+++ src/main/java/com/exchange/exchangeratechallenge/ExchangeRateChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.exchange.exchangeratechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRateChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exchange/exchangeratechallenge/ExchangeRateChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchange.exchangeratechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
